937d87d943acc9da3d2ee38533b2ee5700d9a012
jinghewang <jinghewang@163.com>
2016-07-14 14:31:59 +0800

    dsds

--- app/src/main/java/com/hbdworld/myinternetexplorer/MainActivity.java
+++ app/src/main/java/com/hbdworld/myinternetexplorer/MainActivity.java
@@ -1,24 +1,28 @@
 package com.hbdworld.myinternetexplorer;
 
+import android.content.Context;
 import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 
     private WebView mWebView;
-
+    EditText editText =null;
     private static final String TAG = "hbd";
-
+    String homeUrl = "http://www.hbdfood.com/company/restaurant/shopInfo?shop_id=2902&table=2216";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +30,15 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //--
+        //String url = "file:///android_asset/index.html";
+        String url = "http://www.hbdfood.com/company/restaurant/shopInfo?shop_id=2902&table=2216";
+        //String url = "http://www.baidu.com/";
+
+        editText = (EditText)this.findViewById(R.id.url);
+        editText.setText(url);
 
         //--
         final WebView mWebView = (WebView) findViewById(R.id.webview);
-        final ProgressBar bar = (ProgressBar)findViewById(R.id.progressBar);
         WebSettings settings = mWebView.getSettings();
         settings.setJavaScriptEnabled(true);
         settings.setDomStorageEnabled(true);
@@ -37,22 +46,10 @@ public class MainActivity extends AppCompatActivity {
         mWebView.setWebChromeClient(new WebChromeClient(){
             @Override
             public void onProgressChanged(WebView view, int newProgress) {
-                if (newProgress == 100) {
-                    bar.setVisibility(View.INVISIBLE);
-                } else {
-                    if (View.INVISIBLE == bar.getVisibility()) {
-                        bar.setVisibility(View.VISIBLE);
-                    }
-                    bar.setProgress(newProgress);
-                }
                 super.onProgressChanged(view, newProgress);
             }
         });
 
-
-        //String url = "file:///android_asset/index.html";
-        String url = "http://www.hbdfood.com/company/restaurant/shopInfo?shop_id=2902&table=2216";
-        //String url = "http://www.baidu.com/";
 
         mWebView.setWebViewClient(new WebViewClient(){
             @Override
@@ -74,24 +71,63 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mWebView.loadUrl(url);
 
-
-        Button btn = (Button)this.findViewById(R.id.btn);
-        btn.setOnClickListener(new View.OnClickListener() {
+        Button homeBtn = (Button)this.findViewById(R.id.home_btn);
+        homeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mWebView.loadUrl("javascript:test('wjh')");
+                editText.setText(homeUrl);
+                mWebView.loadUrl(homeUrl);
+                afterExcute();
             }
         });
 
-        Button btn2 = (Button)this.findViewById(R.id.btn2);
-        btn2.setOnClickListener(new View.OnClickListener() {
+
+        Button goBtn = (Button)this.findViewById(R.id.go_btn);
+        goBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mWebView.loadUrl("javascript:test2('lht')");
+                String url = editText.getText().toString();
+                mWebView.loadUrl(url);
+                afterExcute();
+            }
+        });
+
+        Button preBtn = (Button)this.findViewById(R.id.pre_btn);
+        preBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mWebView.canGoForward())
+                    mWebView.goForward();
+                afterExcute();
+            }
+        });
+
+        Button backBtn = (Button)this.findViewById(R.id.back_btn);
+        backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mWebView.canGoBack())
+                    mWebView.goBack();
+                afterExcute();
+            }
+        });
+
+
+        Button scanBtn = (Button)this.findViewById(R.id.scan_btn);
+        scanBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "scan", Toast.LENGTH_SHORT).show();
             }
         });
 
     }
+
+    private void afterExcute() {
+        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(editText.getWindowToken(), 0);
+    }
+
+
 }
